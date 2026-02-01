0f5156982c265033e7df29d703e36d96496402f8
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-01 21:40:48 +0100

    fix : userMapper

--- backend/src/main/java/com/lakroune/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/lakroune/backend/mapper/UserMapper.java
@@ -1,12 +1,21 @@
 package com.lakroune.backend.mapper;
 
-import com.lakroune.backend.dto.request.RegisterRequest;
-import com.lakroune.backend.dto.response.UserResponse;
-import com.lakroune.backend.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.lakroune.backend.dto.request.RegisterRequest;
+import com.lakroune.backend.dto.response.ProfileResponse;
+import com.lakroune.backend.dto.response.UserResponse;
+import com.lakroune.backend.entity.Profile;
+import com.lakroune.backend.entity.User;
 
 @Mapper(componentModel="spring")
 public interface UserMapper {
-    User  toEntity(RegisterRequest request);
+    User toEntity(RegisterRequest request);
+
+    @Mapping(source = "enterprise.id",  target = "enterpriseId")
+    @Mapping(source = "enterprise.nom", target = "enterpriseName")
     UserResponse toResponse(User user);
+
+    ProfileResponse toResponse(Profile profile);
 }
