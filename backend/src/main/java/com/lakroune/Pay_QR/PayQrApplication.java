643a58918c5ef08f7f8ffee89997124a80489c00
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-04 20:53:57 +0100

    feat: add project structure

---
+++ backend/src/main/java/com/lakroune/Pay_QR/PayQrApplication.java
@@ -0,0 +1,13 @@
+package com.lakroune.Pay_QR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayQrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lakroune/Pay_QR/PayQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakroune.Pay_QR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
