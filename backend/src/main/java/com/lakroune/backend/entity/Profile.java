8227762c0a626dadc6cff4ffaac2e8b58bcfc301
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-06 10:24:55 +0100

    feat :  add  entity  profile  and  enume  for  verifucation  satuts

--- backend/src/main/java/com/lakroune/backend/entity/Profile.java
+++ backend/src/main/java/com/lakroune/backend/entity/Profile.java
@@ -1,4 +1,26 @@
 package com.lakroune.backend.entity;
 
+import com.lakroune.backend.enums.VerificationStatus;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String cin;
+    private Date dateNaissance;
+    private String documentType;
+    private String documentImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private VerificationStatus verificationStatus;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
