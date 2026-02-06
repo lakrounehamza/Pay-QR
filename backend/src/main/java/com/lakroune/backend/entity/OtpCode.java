c96c0d60ab4cee89b193b2084febed0a8d1d4d4a
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-06 11:06:16 +0100

    feat :  add Otp  COde  entity

--- backend/src/main/java/com/lakroune/backend/entity/OtpCode.java
+++ backend/src/main/java/com/lakroune/backend/entity/OtpCode.java
@@ -1,4 +1,20 @@
 package com.lakroune.backend.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class OtpCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String code;
+    private LocalDateTime expirationDate;
+    private Boolean used;
+
+    @ManyToOne
+    private User user;
 }
