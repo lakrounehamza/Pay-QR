ff77c0ea5b1e61431cb4a7da2af5bcfc7e5714ba
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-06 11:05:03 +0100

    feat :  add enterprise  entity

--- backend/src/main/java/com/lakroune/backend/entity/Enterprise.java
+++ backend/src/main/java/com/lakroune/backend/entity/Enterprise.java
@@ -1,4 +1,26 @@
 package com.lakroune.backend.entity;
 
+import com.lakroune.backend.enums.EnterpriseStatus;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nom;
+    private String email;
+    private String telephone;
+
+    @Enumerated(EnumType.STRING)
+    private EnterpriseStatus statut;
+
+    private String createdAt;
+
+    @OneToMany(mappedBy = "enterprise")
+    private List<User> users;
 }
