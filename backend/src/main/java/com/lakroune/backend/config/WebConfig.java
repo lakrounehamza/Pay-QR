ac2dbfd7e32f9fdafbb0beead06baacb6ed3ee28
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-08 22:03:45 +0100

    feat(config) : add  filter for http

--- backend/src/main/java/com/lakroune/backend/config/WebConfig.java
+++ backend/src/main/java/com/lakroune/backend/config/WebConfig.java
@@ -1,19 +1,71 @@
 package com.lakroune.backend.config;
 
+import com.lakroune.backend.security.AccessDeniedHandlerImpl;
+import com.lakroune.backend.security.AuthTokenFilter;
+import com.lakroune.backend.security.AuthenticationEntryPointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-        httpSecurity.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().permitAll());
+    public AuthTokenFilter authTokenFilter(){
+        return   new AuthTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        if (userDetailsService instanceof UserDetailsPasswordService) {
+            provider.setUserDetailsPasswordService((UserDetailsPasswordService) userDetailsService);
+        }
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+    @Bean
+    public AuthenticationEntryPointImpl authenticationEntryPoint() {
+        return new AuthenticationEntryPointImpl();
+    }
+    @Bean
+    public AccessDeniedHandlerImpl accessDeniedHandler(){
+        return new AccessDeniedHandlerImpl();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity  httpSecurity){
+        httpSecurity.csrf(csrf ->  csrf.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->
+                        auth
+                                .requestMatchers("/api/auth/signin","/api/auth/signup").permitAll()
+                                .requestMatchers("/api/auth/logout").authenticated()
+                                .anyRequest().permitAll()).exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint())
+                        .accessDeniedHandler(accessDeniedHandler())
+                )
+                .authenticationProvider(daoAuthenticationProvider());
+        httpSecurity.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
     @Bean
