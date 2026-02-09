bd4fc7b450da952bf8477528815687fe5091d0ae
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-09 15:46:00 +0100

    feat : fix  auth controller

--- backend/src/main/java/com/lakroune/backend/controller/AuthController.java
+++ backend/src/main/java/com/lakroune/backend/controller/AuthController.java
@@ -1,12 +1,7 @@
 package com.lakroune.backend.controller;
 
-import com.lakroune.backend.dto.request.LoginRequest;
-import com.lakroune.backend.dto.request.RegisterRequest;
-import com.lakroune.backend.dto.response.LoginResponse;
-import com.lakroune.backend.dto.response.UserResponse;
-import com.lakroune.backend.service.IAuthService;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,23 +9,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.lakroune.backend.dto.request.LoginRequest;
+import com.lakroune.backend.dto.request.RegisterRequest;
+import com.lakroune.backend.dto.response.LoginResponse;
+import com.lakroune.backend.dto.response.UserResponse;
+import com.lakroune.backend.service.IAuthService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/auth")
 public class AuthController {
-    private IAuthService authService;
+
+    private final IAuthService authService;
+
     @PostMapping("/signup")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request){
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request));
     }
+
     @PostMapping("/signin")
-    public  ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
-        return  ResponseEntity.status(HttpStatus.OK).body(authService.login(request));
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(request));
     }
+
     @PostMapping("/logout")
-    public  ResponseEntity<Map> logout(HttpServletRequest reques){
-        return  ResponseEntity.status(HttpStatus.OK).body(authService.logout(reques));
+    public ResponseEntity<Map<String, String>> logout(HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.logout(request));
     }
 }
