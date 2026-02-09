835ff7c8fba17138d150b40650991826642a9b49
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-09 15:01:33 +0100

    refactor: update Enterprise entity

--- backend/src/main/java/com/lakroune/backend/entity/Enterprise.java
+++ backend/src/main/java/com/lakroune/backend/entity/Enterprise.java
@@ -1,12 +1,15 @@
 package com.lakroune.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lakroune.backend.enums.EnterpriseStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,10 +29,18 @@ public class Enterprise {
     private String telephone;
 
     @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(50)")
     private EnterpriseStatus statut;
 
-    private String createdAt;
+    private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "enterprise")
+    @JsonIgnore
+    @ToString.Exclude
     private List<User> users;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
