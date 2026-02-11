a472e05c60ed1103c8f7b73d7ef7892e51120467
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-11 21:45:26 +0100

    feat: notification mail service

---
+++ backend/src/main/java/com/lakroune/backend/service/IEmailService.java
@@ -0,0 +1,5 @@
+package com.lakroune.backend.service;
+
+public interface IEmailService {
+    void sendEmail(String to, String subject, String body);
+}
--- backend/src/main/java/com/lakroune/backend/service/impl/EmailService.java
+++ backend/src/main/java/com/lakroune/backend/service/impl/EmailService.java
@@ -1,11 +1,12 @@
 package com.lakroune.backend.service.impl;
+import com.lakroune.backend.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailService {
+public class EmailService  implements IEmailService {
 
     @Autowired
     private JavaMailSender mailSender;
