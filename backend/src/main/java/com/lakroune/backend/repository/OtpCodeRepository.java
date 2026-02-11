4e8c50d50b348db0971fe89a18575527c5a9167d
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-11 21:43:59 +0100

    feat : otp repository

--- backend/src/main/java/com/lakroune/backend/repository/OtpCodeRepository.java
+++ backend/src/main/java/com/lakroune/backend/repository/OtpCodeRepository.java
@@ -4,9 +4,17 @@ import com.lakroune.backend.entity.OtpCode;
 import com.lakroune.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OtpCodeRepository extends JpaRepository<OtpCode, UUID> {
+
     Optional<OtpCode> findTopByUserAndCodeAndUsedFalseOrderByExpirationDateDesc(User user, String code);
+
+    
+    Optional<OtpCode> findTopByUserAndUsedFalseOrderByExpirationDateDesc(User user);
+
+    
+    void deleteByExpirationDateBefore(LocalDateTime dateTime);
 }
