98e195075d606ea97f209daf84901895c0de598e
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-15 16:41:57 +0100

    feat: add operation controller

---
+++ backend/src/main/java/com/lakroune/backend/controller/OperationController.java
@@ -0,0 +1,43 @@
+package com.lakroune.backend.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lakroune.backend.dto.request.CreateOperationRequest;
+import com.lakroune.backend.dto.response.OperationResponse;
+import com.lakroune.backend.service.IOperationService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/operation")
+@RequiredArgsConstructor
+public class OperationController {
+
+    private final IOperationService operationService;
+
+    @PostMapping
+    public ResponseEntity<OperationResponse> create(@Valid @RequestBody CreateOperationRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(operationService.save(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OperationResponse>> getAllOperation() {
+        return ResponseEntity.status(HttpStatus.OK).body(operationService.getAllOperation());
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<List<OperationResponse>> getByAccountId(@PathVariable UUID accountId) {
+        return ResponseEntity.status(HttpStatus.OK).body(operationService.getByAccountId(accountId));
+    }
+}
