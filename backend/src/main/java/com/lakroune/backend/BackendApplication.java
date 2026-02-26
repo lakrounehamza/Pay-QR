41897e221a7ae53288f74bd85b8488f6357db3e9
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-26 16:21:30 +0100

    feat : add  configuration env

--- backend/src/main/java/com/lakroune/backend/BackendApplication.java
+++ backend/src/main/java/com/lakroune/backend/BackendApplication.java
@@ -3,10 +3,18 @@ package com.lakroune.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+			.filename(".env")
+			.ignoreIfMalformed()
+			.ignoreIfMissing()
+			.systemProperties()
+			.load();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
