84292c2040ef44f47d2c4f2c5ac34430371c2a9d
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-28 14:16:24 +0100

    fix :  fix OTP repository

--- backend/src/main/java/com/lakroune/backend/repository/OtpCodeRepository.java
+++ backend/src/main/java/com/lakroune/backend/repository/OtpCodeRepository.java
@@ -1,9 +1,12 @@
 package com.lakroune.backend.repository;
 
 import com.lakroune.backend.entity.OtpCode;
+import com.lakroune.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OtpCodeRepository extends JpaRepository<OtpCode, UUID> {
+    Optional<OtpCode> findTopByUserAndCodeAndUsedFalseOrderByExpirationDateDesc(User user, String code);
 }
