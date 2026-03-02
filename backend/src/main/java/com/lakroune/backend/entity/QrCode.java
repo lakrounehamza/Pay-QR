3c6ddf046a5ea926bd569794d51eb00abc27103e
lakrounehamza <lakrounehamza10@gmail.com>
2026-03-02 17:02:11 +0100

    feat(qrcode): add qrCodeImageUrl field and clean up import

--- backend/src/main/java/com/lakroune/backend/entity/QrCode.java
+++ backend/src/main/java/com/lakroune/backend/entity/QrCode.java
@@ -6,13 +6,22 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lakroune.backend.enums.OwnerType;
-import lombok.ToString;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
@@ -33,6 +42,7 @@ public class QrCode {
     private LocalDateTime dateExpiration;
     private Boolean isUsed;
     private String createdAt;
+    private String qrCodeImageUrl;
 
     @ManyToOne
     @JoinColumn(name = "account_id")
